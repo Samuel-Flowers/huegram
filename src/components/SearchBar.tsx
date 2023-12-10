7a06c0c8b219adda31e525a8d3ef02944acc9ab3
sflowers51 <samuel.flowers@students.fhu.edu>
2023-12-09 23:09:50 -0600

    bug fix attempt

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,34 +1,52 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch as faSearch } from '@fortawesome/free-solid-svg-icons';
-import { SearchResult } from './SearchResult';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
-export const SearchBar = ({setResults}: {setResults: string}) => {
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const handleChange = (value: string) => {
-      setSearchTerm(value);
-    
-      // Use the value parameter directly in the filter function
-      fetchData(value);
-    };
-    
-    const fetchData = (term: string) => {
-      fetch("sampleData.json")
-        .then((response) => response.json())
-        .then((json) => {
-            const results = json.filter((color: { color: string }) => {
-            return color && color.color && color.color.toLowerCase().includes(term.toLowerCase());
-          });
-            setResults(results);
-        });
-    };
-    return ( 
-        <div className="input-wrapper">
-            <input type="text" placeholder="Search" value={searchTerm} onChange={(e) => handleChange(e.target.value)}/>
-            <FontAwesomeIcon icon={faSearch}/>
-        </div>
-     );
+interface Color {
+  color: string;
 }
+
+export const SearchBar = ({ setResults }: { setResults: (results: Color[]) => void }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (value: string) => {
+    setSearchTerm(value);
+
+    // Use the value parameter directly in the filter function
+    fetchData(value);
+  };
+
+  const fetchData = (term: string) => {
+    fetch("sampleData.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((json) => {
+        const results = json.filter((color: { color: string }) => {
+          return color && color.color && color.color.toLowerCase().includes(term.toLowerCase());
+        });
+        setResults(results);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setResults([]); // Set results to an empty array in case of an error
+      });
+  };
+
+  return (
+    <div className="input-wrapper">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+      <FontAwesomeIcon icon={faSearch} />
+    </div>
+  );
+};
  
--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,4 +1,6 @@
-export const SearchResult = ({result}) => {
+import React from 'react'
+
+export const SearchResult = ({result}: {result: any}) => {
   return (
     <div>{result.color}</div>
   )
