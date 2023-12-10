5e6b54aa5409c7e0e7a22ad448f971e1e500c91b
sflowers51 <samuel.flowers@students.fhu.edu>
2023-12-09 22:09:32 -0600

    attempting bug fixed

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,27 +1,27 @@
 import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch as faSearch } from '@fortawesome/free-solid-svg-icons';
+import { SearchResult } from './SearchResult';
 
 
-
-export const SearchBar = ({setResults}) => {
+export const SearchBar = ({setResults}: {setResults: string}) => {
     const [searchTerm, setSearchTerm] = useState('');
 
-    const handleChange = (value) => {
+    const handleChange = (value: string) => {
       setSearchTerm(value);
     
       // Use the value parameter directly in the filter function
       fetchData(value);
     };
     
-    const fetchData = (term) => {
+    const fetchData = (term: string) => {
       fetch("sampleData.json")
         .then((response) => response.json())
         .then((json) => {
-          const results = json.filter((color) => {
+            const results = json.filter((color: { color: string }) => {
             return color && color.color && color.color.toLowerCase().includes(term.toLowerCase());
           });
-          setResults(results);
+            setResults(results);
         });
     };
     return ( 
--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const SearchResult = ({result}) => {
   return (
     <div>{result.color}</div>
